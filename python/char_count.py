eae5ca602524d6a77eb8cb7fe912e8f356f2ab9e
stesun2 <ss27@illinois.edu>
2021-05-30 16:43:46 -0500

    work in progress

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,10 @@
+# Using dictionary to count the number of char
+
 def char_count(str):
+
+    return str
+
   # Your code here
+  
+print(char_count("aaabbc"))
+print(char_count("an apple a day will keep the doctor away"))
